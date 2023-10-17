1fc3eae97858afc4ed368ca5269d3c264d79cb73
k_hitarth <hitarth.k@inheritx.com>
2023-10-17 12:57:19 +0530

    Added functionality to deploy

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-import About from './components/About';
+// import About from './components/About';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
@@ -37,18 +37,20 @@ function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router> */}
 
-        <Navbar title='Navbar' aboutText='About TextUtils' mode={mode} toggleMode={toggleMode}></Navbar>
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path='/about' element={<About />} />
-            <Route exact path='/' element={<Textform showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />} />
-          </Routes>
-        </div>
+      {/* <Navbar title='Navbar' aboutText='About' mode={mode} toggleMode={toggleMode}></Navbar> */}
+      <Navbar title='Navbar' aboutText='About' mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact path='/about' element={<About />} /> */}
+        {/* <Route exact path='/' element={<Textform showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />} /> */}
+        {/* </Routes> */}
+        <Textform showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />
+      </div>
 
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
